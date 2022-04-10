cb35bdb1646ae09d1243901c86b6494bd667137e
Max Philips <mphilips890@gmail.com>
2022-04-10 18:41:29 -0500

    CRUD drinks

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -6,4 +6,45 @@ class DrinksController < ApplicationController
   def show
     @drink = Drink.find(params[:id])
   end
+
+  def new
+    @drink = Drink.new
+  end
+
+  def create
+    @drink = Drink.new(drink_params)
+
+    if @drink.save
+      redirect_to @drink
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @drink = Drink.find(params[:id])
+  end
+
+  def update
+    @drink = Drink.find(params[:id])
+
+    if @drink.update(drink_params)
+      redirect_to @drink
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @drink = Drink.find(params[:id])
+    @drink.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def drink_params
+    params.require(:drink).permit(:name, :description, :preparation)
+  end
 end
--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,5 @@
 class Drink < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation, presence: true
 end
