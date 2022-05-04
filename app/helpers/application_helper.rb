8a2d4948ffeacdb16d403b60849289e4b9e1cc70
Max Philips <mphilips890@gmail.com>
2022-05-03 21:08:41 -0500

    More styling and make header nav active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_class(path)
+    request.path == path ? 'active' : ''
+  end
+
+  def active_link_to(name, path)
+    link_to(name, path, class: "nav-link #{active_class(path)}")
+  end
 end
